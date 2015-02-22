94a3394fcab660b4a740b1eea69d1e30f69651bf
John Ramey <johnramey@gmail.com>
2015-02-22 11:21:55 -0600

    Fixed docs to remove installation warning.

--- R/pocketknife.r
+++ R/pocketknife.r
@@ -1,7 +1,5 @@
 #' A collection of useful utility functions
 #'
-#' @section Package Description:
-#'
 #' The R package \code{pocketknife} contains a variety of useful utility
 #' functions to simplify and to enhance common data-manipulation tasks in R.
 #'
